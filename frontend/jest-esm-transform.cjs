da8ed184f334326dde401a8a3f1a490f4644b0af
NihilTH <gamertheglas@gmail.com>
2026-09-10 05:42:53 +0200

    Support Babel Jest default export in transformer

--- frontend/jest-esm-transform.cjs
+++ frontend/jest-esm-transform.cjs
@@ -1,4 +1,5 @@
-module.exports = require("babel-jest").createTransformer({
+const babelJest = require("babel-jest");
+module.exports = (babelJest.default || babelJest).createTransformer({
   babelrc: false, configFile: false,
   plugins: [require.resolve("@babel/plugin-transform-modules-commonjs")],
 });
